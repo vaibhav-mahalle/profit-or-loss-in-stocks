2f1b4a278f50aa25f0f79c0208bb1d2736d92219
Vaibhav Kishor Mahalle <vaibhavmahalle95@gmail.com>
2021-09-07 11:02:14 +0530

    input taking done and eventlistener added

---
+++ app.js
@@ -0,0 +1,13 @@
+const initialPrice = document.querySelector("#stock-price");
+const number = document.querySelector("#number");
+const finalPrice = document.querySelector("#final-price");
+const inputBtn = document.querySelector("#enter");
+
+function clickHandler(){
+    // console.log('clicked');
+
+}
+
+
+inputBtn.addEventListener('click',clickHandler)
+
