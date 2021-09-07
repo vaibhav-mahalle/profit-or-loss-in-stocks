a5ef953d928beffae8081b36b0e5ccaa573e24c3
Vaibhav Kishor Mahalle <vaibhavmahalle95@gmail.com>
2021-09-07 15:10:59 +0530

    negative values handled

--- app.js
+++ app.js
@@ -7,63 +7,58 @@ const output = document.querySelector("#output-box");
 var profit = 0;
 var noofstocks = 0;
 var loss = 0;
-var profitPercentage=0;
-var lossPercentage= 0;
-var totalvalue=0;
+var profitPercentage = 0;
+var lossPercentage = 0;
+var totalvalue = 0;
 
 
-function calculatePercentage(difference)
-{
-    var percentage = (difference/Number(initialPrice.value))*100 ;
+function calculatePercentage(difference) {
+    var percentage = (difference / Number(initialPrice.value)) * 100;
     percentage = Math.round(percentage);
     return percentage;
 }
 
-function calculateDifference()
-{
+function calculateDifference() {
     var difference = Number(finalPrice.value) - Number(initialPrice.value);
     console.log(difference);
 
-    if(difference>0)
-    {
-        profit = difference * noofstocks ;
+    if (difference > 0) {
+        profit = difference * noofstocks;
 
         profitPercentage = calculatePercentage(difference);
         console.log(profitPercentage);
-       
-    }
-    else
-    {
-        loss = Math.abs(difference)*noofstocks;
+
+    } else {
+        loss = Math.abs(difference) * noofstocks;
         console.log(loss);
         lossPercentage = calculatePercentage(Math.abs(difference));
         console.log(lossPercentage);
-       
+
     }
 }
 
-function clickHandler(){
-     noofstocks=parseInt(amount.value);
-     
-    calculateDifference();
-    
-    if(profit>0)
-    {
-        console.log('hello');
-        output.innerText = `Jab Profit Dikhta hai, toh har koi jhukta hai💰💰:Profit= ${profit}rs ProfitPercentage= ${profitPercentage}%`;
-    }
-    if(loss>0)
-    {
-        console.log('hello loss');
-        output.innerText = `Emotion mein insaan hamesa galti karta hai!:Loss=${loss}rs  LossPercentage=${lossPercentage}% `;
-    }
-    if(profit===0 && loss===0)
-    {
-        output.innerText = `kuch nhi mila re Baba tereko!!`;
+function clickHandler() {
+    noofstocks = parseInt(amount.value);
+
+    if (Number(initialPrice.value) < 0 || Number(initialPrice.value) < 0 || noofstocks < 0) {
+        output.innerText = "One or more value is negative.Please enter positive values.";
+    } else {
+        calculateDifference();
+
+        if (profit > 0) {
+
+            output.innerText = `Jab Profit Dikhta hai, toh har koi jhukta hai💰💰:Profit= ${profit}rs ProfitPercentage= ${profitPercentage}%`;
+        }
+        if (loss > 0) {
+            output.innerText = `Emotion mein insaan hamesa galti karta hai!:Loss=${loss}rs  LossPercentage=${lossPercentage}% `;
+        }
+        if (profit === 0 && loss === 0) {
+            output.innerText = `kuch nhi mila re Baba tereko!!`;
+        }
+
     }
 
 }
 
 
-inputBtn.addEventListener('click',clickHandler)
-
+inputBtn.addEventListener('click', clickHandler)
